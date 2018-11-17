c5736ac8993f557c9e7520f860babb6565b06bbe
jayasenapm <pmjayasena123456@gmail.com>
2018-11-18 00:12:13 +0530

    login information
    login informetion

--- resources/views/form.php
+++ resources/views/form.php
@@ -1,6 +1,6 @@
 <html>
    <body>
-      <h1>Form For Registration</h1>
+      <h1>Form For Login Informetion</h1>
       <?php
          echo Form::open(array('url' => 'foo/bar'));
             echo Form::text('username','Username');
